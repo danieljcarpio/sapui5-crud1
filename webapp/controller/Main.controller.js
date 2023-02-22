705f8e2151b8bd5394523b4a97163c22b09e3a5a
Daniel José Carpio Contreras <daniel.carpio@seidor.com>
2023-02-22 00:25:18 +0000

    Added logica para retornar lista de Productos

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,14 +1,29 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageBox) {
         "use strict";
 
         return Controller.extend("com.seidor.ns.sapui5crud1.controller.Main", {
             onInit: function () {
+
+                var that = this;
+                var odataModel = new sap.ui.model.odata.v2.ODataModel("/V2/(S(gpjcelaleuaxoofac1repdts))/OData/OData.svc/");
+                odataModel.read("/Products", {
+                    success: function (oData, oResponse) {
+                        console.log("oData =>", oData);
+                        MessageBox.success("Success");
+                    },
+                    error: function (oError) {
+                        console.log("oError =>", oError);
+                        MessageBox.error("Error");
+                    }
+                });
+                this.getView().setModel(odataModel);
 
             }
         });
